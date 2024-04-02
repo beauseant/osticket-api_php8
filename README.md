1d42a1460b9e14b985ac9baea0f72885f6622b48
beauseant <sblanco@tsc.uc3m.es>
2024-04-02 16:30:24 +0200

    api funcionando y test en python
    Signed-off-by: beauseant <sblanco@tsc.uc3m.es>

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # osticket-api_php8
 Modificación del repositorio osticket-api para que funcione sobre PHP8
+
+Se trata de una modificación de: https://bmsvieira.github.io/osticket-api/ , para que funcione sobre PHP8.
+
+Se incluye un pequeño script en python que añade y recupera un ticket creado.
+
+#Uso:
+
+Then, go to ost_wbs > config.php and change the DB credentials and the Table prefix.
+
+Use the following base URL:
+
+{YOUR-DOMAIN}/upload/ost_wbs/
+
+https://bmsvieira.gitbook.io/osticket-api/get-started/configuration
+
---
+++ testAPI.py
@@ -0,0 +1,70 @@
+import requests
+
+url="https://192.168.151.254/osticket/ost_wbs/"
+key = "XXXXXXXXX"
+verifyCert = False
+
+#auth header:
+headers = {"apikey":key}
+
+
+
+
+ticketDataCreate={
+    "query":"ticket",
+    "condition":"add",
+    "parameters":{
+    "title":"No funciona nada",
+    "subject":"furioso con el mundo",
+    "user_id": 2,
+    "priority_id":2,
+    "status_id":1,
+    "dept_id":1,
+    "sla_id":1,
+    "topic_id":1
+    }
+}
+
+
+
+def createTicket ():
+
+    r = requests.post(url=url,json=ticketDataCreate,headers=headers, verify = verifyCert)
+
+    print('INFO :: Request Status Code: ', r.status_code)
+
+    if (r.status_code < 200 or r.status_code >= 300):
+        return False
+
+    print('INFO :: Ticket Number: ', r.json())
+    return r.json()['data']
+
+
+def getTicket (id):
+
+
+    dataget = {
+    "query":"ticket",
+    "condition":"specific",
+    "parameters":{
+        "id":id
+        }
+    }
+
+    r = requests.get(url=url,json=dataget,headers=headers,  verify=verifyCert)
+    response = r
+    print('INFO :: Request Status Code: ', r.status_code)
+
+    print (response)
+    if (r.status_code < 200 or r.status_code >= 300):
+        print (r)
+        return False
+
+    print('INFO :: Ticket info: ', r.json())
+    return True
+	
+	
+ticketId = createTicket()
+
+getTicket( ticketId )
+
